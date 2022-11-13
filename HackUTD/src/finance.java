24526dcc27527ae5d80b2af10a54c966ff281b09
Emelie4 <108645700+Emelie4@users.noreply.github.com>
2022-11-12 19:51:43 -0600

    Added a label and final dimensions of program

--- HackUTD/src/finance.java
+++ HackUTD/src/finance.java
@@ -1,11 +1,16 @@
 import java.awt.*;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class finance {
 	
 	JFrame newFrame;
 	Container newCon;
+	JPanel titleNamePanelOutline;
+	JPanel titleNamePanelInner;
+	JLabel titleNameLabel;
 	
 
 	public static void main(String[] args) {
@@ -20,11 +25,18 @@ public class finance {
 	public finance() {
 		newFrame = new JFrame("FinanceYouWell");
 		newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		newFrame.setSize(600,600);
+		newFrame.setSize(800,600);
 		newFrame.setLocationRelativeTo(null);
 		newFrame.getContentPane().setBackground(Color.WHITE);
 		newFrame.setLayout(null);
 		newCon = newFrame.getContentPane();
+		newCon = newFrame.getContentPane();
+		titleNamePanelOutline = new JPanel();
+		titleNameLabel = new JLabel();
+		titleNamePanelOutline.setBounds(100,100,600,150);
+		titleNamePanelOutline.setBackground(Color.BLACK);
+		
+		newCon.add(titleNamePanelOutline);
 		
 		
 		
