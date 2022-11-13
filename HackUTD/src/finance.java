07bce1974146137b78871d9eb371b9e93f2e2e4b
Emelie4 <108645700+Emelie4@users.noreply.github.com>
2022-11-12 18:46:44 -0600

    New frame created

--- HackUTD/src/finance.java
+++ HackUTD/src/finance.java
@@ -1,10 +1,31 @@
 import java.awt.*;
 
+import javax.swing.JFrame;
+
 public class finance {
+	
+	JFrame newFrame;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new finance();
+		
 
-	}
+	}//end of main
+	
+	
+	
+	public finance() {
+		newFrame = new JFrame("FinanceYouWell");
+		newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		newFrame.setSize(600,600);
+		newFrame.setLocationRelativeTo(null);
+		
+		
+		
+		
+		
+		newFrame.setVisible(true);
+	}//end of finance constructor
 
-}
+}//end of class
